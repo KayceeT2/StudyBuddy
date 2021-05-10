a6f0528dac8b8a54b7bd6383dc6bde3c56835023
Kaycee <KayceeT2>
2021-05-10 12:36:50 -0400

    Initial Commit

---
+++ StudyBuddy/Controllers/QuestionViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class QuestionViewController: UIViewController {
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func answerChoicesPressed(_ sender: UIButton) {
+        
+    }
+}
